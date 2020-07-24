d37bfe2895b98255ce5cd9e2033b92069e2ea84b
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-24 03:19:29 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
